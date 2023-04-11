5f2bb0be717cb350fa3b5026179e511f7b885727
santharam243 <santharam243@gmail.com>
2023-04-11 11:12:36 +0530

    lombok initial commit

---
+++ src/main/java/com/training/springlombok/SpringLombokApplication.java
@@ -0,0 +1,13 @@
+package com.training.springlombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLombokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/springlombok/SpringLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.springlombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
